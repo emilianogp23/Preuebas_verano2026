92f7ee8b2abc2d8a9265384be14636912ce42730
emilianogp23 <emigarper@outlook.com>
2026-06-15 16:22:12 -0600

    Trayectorias(1.0) PID

--- controllers/Controlador_principal/trayectoria.py
+++ controllers/Controlador_principal/trayectoria.py
@@ -6,9 +6,9 @@ class control_trayectoria:
     def __init__(self):
 
         self.fov=0.87
-        ladox=1.0  #0.4
-        ladoy=1.0
-        ladoz=1.3+0.7
+        ladox=1.5  #0.4
+        ladoy=1.5
+        ladoz=1.3+0.9
         self.altura_vuelo=ladoz/2
         self.perimetro=2*(ladox+ladoy)
         self.area=(ladox*ladoy)
