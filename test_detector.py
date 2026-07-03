1333d5d1008977d872153ff839d93c121015ed03
Lalo-Ynz <jeduardoyanez03@gmail.com>
2026-07-03 00:13:38 -0600

    Delete test_detector.py

--- test_detector.py
+++
@@ -1,7 +0,0 @@
-import os
-from yolo_detector import YoloDetector
-
-detector = YoloDetector()
-puntaje = detector.imagenes_capturadas()
-print(f"Puntaje total: {puntaje}")
-print(f"Mejores puntajes: {detector.mejores_puntajes}")
